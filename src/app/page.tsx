2b2852d961662a878c8d2726ca7b61e82c18057e
unknown <saummanzoor49@gmail.com>
2023-10-21 00:17:58 +0500

    sanity-schemas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import { client } from '@/lib/sanityClient'
 import getImgUrl from '../../sanity/lib/getImgUrl';
+import { Image as IImage } from 'sanity';
+import Image from 'next/image'
 
 interface IProduct{
   title:string,
+  _id:string,
+  price:string,
   description:string,
-  image:any,
-  imgUrl:string|undefined
+  image:IImage,
+  imgUrl:string|'',
+  category:{
+    name:string
+  }
 
 }
 
@@ -32,12 +39,21 @@ export default async function Home() {
 return d
   })
   
-  console.log(data);
   return (
    <>
+   <div className="grid grid-cols-[repeat(3,auto)] justify-center gap-x-10 ">
+
   {
-    data.map((d)=>(<h1 key={d.description}>{d.title}</h1>))
+    data.map((d)=>(
+      <div key={d._id}>
+        <Image src={d.imgUrl} alt='Product' height={200} width={300} className='max-h-[200px] object-cover object-top' />
+        <h1>{d.title}</h1>
+        <h3>${d.price}</h3>
+        <button className='border py-2 px-6 rounded bg-blue-600 text-white '>Add to cart</button>
+      </div>
+    ))
   }
+  </div>
    </>
   )
 }
